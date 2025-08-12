6f4c5284abfc49e9f07c78c0fac7e77b93bb0b6c
@jai0710 <kottu.venu@kiwitech.com>
2025-08-12 09:56:28 -0400

    First spring setup

---
+++ src/main/java/com/spring/techie/sumanth/CrudOperationSpringTechieApplication.java
@@ -0,0 +1,13 @@
+package com.spring.techie.sumanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationSpringTechieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationSpringTechieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/techie/sumanth/CrudOperationSpringTechieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.techie.sumanth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationSpringTechieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
