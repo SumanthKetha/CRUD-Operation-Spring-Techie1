53771510f8d63fa87cf1b9c50a0f52380c3d64ca
springtechie <springframeworkdeveloper@gmail.com>
2025-08-13 19:32:37 +0530

    RAJA-1: created crud operations.

---
+++ src/main/java/com/spring/techie/sumanth/controllers/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.spring.techie.sumanth.controllers;
+
+import com.spring.techie.sumanth.model.Employee;
+import com.spring.techie.sumanth.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/save")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("/get/id/{id}")
+    public Employee findEmployee(@PathVariable Integer id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("/update")
+    public String updateEmployee(@RequestBody  Employee employee) {
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public String removeEmployee(@PathVariable Integer id) {
+        return employeeService.deleteEmployeeById(id);
+    }
+}
