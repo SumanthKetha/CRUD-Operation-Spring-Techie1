d6865eb3d4d9c23d61d72f1c7b26e27504406b48
springtechie <springframeworkdeveloper@gmail.com>
2025-08-15 19:34:23 +0530

    RAJA-1: Theory About IOC

--- src/main/java/com/spring/techie/sumanth/CrudOperationSpringTechieApplication.java
+++ src/main/java/com/spring/techie/sumanth/CrudOperationSpringTechieApplication.java
@@ -1,13 +1,38 @@
 package com.spring.techie.sumanth;
 
+import com.spring.techie.sumanth.controllers.EmployeeController;
+import com.spring.techie.sumanth.impl.EmployeeServiceImpl;
+import com.spring.techie.sumanth.model.Employee;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class CrudOperationSpringTechieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudOperationSpringTechieApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CrudOperationSpringTechieApplication.class, args);
+		EmployeeController bean = run.getBean(EmployeeController.class);
+		System.out.println(bean.hashCode());
+		EmployeeController bean1 = (EmployeeController)run.getBean("employeeController");
+		System.out.println(bean1.hashCode());
+		System.out.println(run.containsBean("employeeService"));
+		EmployeeServiceImpl employeeService = run.getBean( EmployeeServiceImpl.class);
+		System.out.println(employeeService.getClass().getSimpleName());
+
+	}
+
+	@PostConstruct
+	public void welcome() {
+		System.out.println("Hello Welcome to Application");
+	}
+
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Bye!!");
 	}
 
 }
